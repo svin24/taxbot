014ef5a6b35f6267480192fb60a411671c1a2e84
svin24 <svin@dismail.de>
2024-06-20 09:53:47 +0300

    step3-ish: basic AI integration

--- FlaskTaxBot/tax_assistant.py
+++ FlaskTaxBot/tax_assistant.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, render_template,request
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 from sqlalchemy.exc import IntegrityError
+import openai
 
 class TaxBase(DeclarativeBase):
     pass
@@ -10,6 +11,7 @@ db = SQLAlchemy(model_class=TaxBase)
 taxBot = Flask(__name__)
 taxBot.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///taxbot_data.db'
 
+## database setup araea
 db.init_app(taxBot)
 
 class TaxData(db.Model):
@@ -26,6 +28,26 @@ class TaxData(db.Model):
 with taxBot.app_context():
     db.create_all()
 
+# OpenAI - TaxBot integration
+with open('api_key','r') as file:
+    openai.api_key = file.read().strip()
+
+def get_ai_response(prompt):
+    try:
+        response = openai.chat.completions.create(
+            model = "gpt-4o",
+            messages = [
+                {"role": "system", "content": """You are a digital assistant designed to help people do their Taxes. You will recieve messages in the following format income=Number expenses=Number, prompt=Text. Sometimes income and expenses might be undefined, if that is the case ignore them completely and focus on the prompt."""},
+                {"role": "user", "content": prompt }
+            ]
+        )
+        return response.choices[0].message.content
+    except Exception:
+        return "Error getting response from taxbot"
+
+
+
+# Flask routing
 @taxBot.route('/')
 def home():
     return render_template("index.html")
@@ -35,15 +57,15 @@ def submit():
     income = request.form['income']
     expenses = request.form['expenses']
     prompt = request.form['prompt']
-
+    
+    # basic database integrity stuff
     try:
         income = float(income)
         expenses = float(expenses)
     except ValueError:
+        # error is handled with an alert box right now. It's fine for an assignment but not a real world program
         return jsonify({'error': 'Income and Expenses must be numbers'}),400
-    # error is handled with an alert box right now. It's fine for an assignment but not a real world program
 
-    # insert data into database
     tax_data = TaxData(income=income,expenses=expenses,prompt=prompt)
     db.session.add(tax_data)
     try:
@@ -52,9 +74,9 @@ def submit():
         db.session.rollback()
         return jsonify({'error': 'Database error occured'}),500
     
+    final_prompt = 'income={} expenses={}, prompt={}'.format(income,expenses,prompt)
 
-    # AI not integrated yet
-    ai_response = "Not Integrated"
+    ai_response = get_ai_response(final_prompt)
 
     return jsonify({
         'income': income,
@@ -64,4 +86,4 @@ def submit():
     })
 
 if __name__ == '__main__':
-    taxBot.run(debug=True)
+    taxBot.run(debug=True)cat text in python
